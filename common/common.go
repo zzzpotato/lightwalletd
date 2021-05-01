95d19ee3f6e45bd950ef0d2469dfe93b570abd65
Potato <taylor.reed16@yahoo.com>
2021-05-01 04:58:34 +0200

    Initial ZeroClassic Support

--- common/common.go
+++ common/common.go
@@ -191,7 +191,7 @@ func GetLightdInfo() (*walletrpc.LightdInfo, error) {
 	}
 	// If the sapling consensus branch doesn't exist, it must be regtest
 	var saplingHeight int
-	if saplingJSON, ok := getblockchaininfoReply.Upgrades["76b809bb"]; ok { // Sapling ID
+	if saplingJSON, ok := getblockchaininfoReply.Upgrades["7a737763"]; ok { // Sapling ID
 		saplingHeight = saplingJSON.ActivationHeight
 	}
 
